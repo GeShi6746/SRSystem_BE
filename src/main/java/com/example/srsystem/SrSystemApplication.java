519b0398e359ad7c25d35cb3c076e3e6aafc6c6d
momomomona <32504354+momomomona@users.noreply.github.com>
2022-05-30 00:01:17 -0500

    Initial project

---
+++ src/main/java/com/example/srsystem/SrSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.srsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/srsystem/SrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
